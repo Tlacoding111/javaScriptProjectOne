65a669a72cb11496aad67d55db1223f8150617d6
tla111 <tlamarketing111@gmail.com>
2019-01-01 14:41:15 -0500

    made changes

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 const myHeading = document.getElementById("myHeading");
 const myButton = document.getElementById("myButton");
 const myTextInput = document.getElementById("myTextInput");
+const myList = document.getElementsByTagName("li");
 
 myButton.addEventListener("click", () => {
     myHeading.style.color = myTextInput.value;
 });
+
+for (let i = 0; i < myList.length; i += 1) {
+    myList[i].style.color = "purple";
+}
+
+const notPurple = document.querySelectorAll(".error");
+
+for (let i = 0; i < notPurple.length; i += 1) {
+    notPurple[i].style.color = "blue";
+}
+const evens = document.querySelectorAll("li:nth-child(even)");
+
+for (let i = 0; i < evens.length; i += 1) {
+    evens[i].style.backgroundColor = "lightgray";
+}
