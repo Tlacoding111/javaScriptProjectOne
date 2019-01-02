d52114a501e4509998825d48d2e635f4084b8be6
tla111 <tlamarketing111@gmail.com>
2019-01-01 19:38:37 -0500

    made changes

--- main.js
+++ main.js
@@ -4,15 +4,51 @@ const myHeading = document.getElementById("myHeading");
 const myButton = document.getElementById("myButton");
 const myTextInput = document.getElementById("myTextInput");
 const myList = document.getElementsByTagName("li");
+const listUl = listDiv.querySelector("ul");
 const addItemInput = document.querySelector("input.addItemInput");
 const addItemButton = document.querySelector("button.addItemButton");
 const groceryItemInput = document.querySelector("input.groceryItemInput");
 const groceryItemButton = document.querySelector("button.groceryItemButton");
 const newItemInput = document.querySelector("input.newItemInput");
 const newItemButton = document.querySelector("button.newItemButton");
-const removeItemButton = document.querySelector("button.removeItemButton");
-const removeSecondItemButton = document.querySelector("button.removeSecondItemButton");
-const wipeItemButton = document.querySelector("button.wipeItemButton");
+
+listUl.addEventListener("click", (event) => {
+    if (event.target.tagName == "BUTTON") {
+        if (event.target.className = "remove") {
+            let li = event.target.parentNode;
+            let ul = li.parentNode;
+            ul.removeChild(li);
+        }
+        if (event.target.className == "up") {
+            let li = event.target.parentNode;
+            let prevLi = li.previousElementsSibling;
+            let ul = li.parentNode;
+            if (prevLi) {
+                ul.insertBefore(li, prevLi)
+            }
+        }
+        if (event.target.className == "down") {
+            let li = event.target.parentNode;
+            let nextLi = li.nextElementsSibling;
+            let ul = li.parentNode;
+            if (nextLi) {
+                ul.insertBefore(nextLi, prevLi)
+            }
+        }
+    }
+})
+
+// listDiv.addEventListener("mouseover", (event) => {
+//     if (event.target.tagName == "LI") {
+//         event.target.textContent = event.target.textContent.toUpperCase();
+//     }
+// });
+// listDiv.addEventListener("mouseout", (event) => {
+//     if (event.target.tagName == "LI") {
+//         event.target.textContent = event.target.textContent.toLowerCase();
+//     }
+// });
+
 
 toggleList.addEventListener("click", () => {
     if (listDiv.style.display == "none") {
@@ -76,20 +112,4 @@ newItemButton.addEventListener("click", () => {
 
 //Remove
 
-// removeItemButton.addEventListener("click", () => {
-//     let ul = document.getElementsByClassName("bringList")[0];
-//     let li = document.querySelector("li:last-child");
-//     ul.removeChild(li);
-// });
 
-// removeSecondItemButton.addEventListener("click", () => {
-//     let ul = document.getElementsByClassName("groceryList")[0];
-//     let li = document.querySelector("li:last-child");
-//     ul.removeChild(li);
-// });
-
-// wipeItemButton.addEventListener("click", () => {
-//     let ul = document.getElementsByClassName("newList")[0];
-//     let li = document.querySelector("li:last-child");
-//     ul.removeChild(li);
-// });
