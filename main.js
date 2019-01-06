a171083e8267d284409bf427444faad68390d956
Tim La <email@example.com>
2019-01-06 16:48:37 -0500

    made changes

--- main.js
+++ main.js
@@ -1,55 +1,20 @@
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
+const myHeading = document.getElementById("myHeading");
+const myButton = document.getElementById("myButton");
+const myTextInput = document.getElementById("myTextInput");
+
+myButton.addEventListener("click", () => {
+    myHeading.style.color = myTextInput.value;
+
+});
+
+const myList = document.getElementsByTagName("li");
+
+for (let i = 0; i < myList.length; i += 1) {
+    myList[i].style.color = "purple";
 }
 
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
+const evens = document.querySelectorAll("li:nth-child(even)");
 
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
+for (let i = 0; i < evens.length; i += 1) {
+    evens[i].style.backgroundColor = "lightgray";
 }
