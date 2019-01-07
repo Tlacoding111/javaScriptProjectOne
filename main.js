d5e0d48b7dc4c7ee4112c0f5beaca83c2c729332
Tim La <email@example.com>
2019-01-06 22:52:56 -0500

    made changes

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const myButton = document.getElementById("myButton");
 const myTextInput = document.getElementById("myTextInput");
 
 myButton.addEventListener("click", () => {
-    myHeading.style.color = myTextInput.value;
+    myHeading.textContent = myTextInput.value;
 
 });
 
@@ -20,17 +20,37 @@ toggleList.addEventListener("click", () => {
     }
 })
 
-const myList = document.getElementsByTagName("li");
+function attachListItemButtons(li) {
+    let up = document.createElement("button");
+    up.className = "up";
+    up.textContent = "Up";
+    li.appendChild(up);
 
-for (let i = 0; i < myList.length; i += 1) {
-    myList[i].style.color = "purple";
+    let down = document.createElement("button");
+    down.className = "down";
+    down.textContent = "Down";
+    li.appendChild(down);
+
+    let remove = document.createElement("button");
+    remove.className = "remove";
+    remove.textContent = "Remove";
+    li.appendChild(remove);
 }
 
-const odd = document.querySelectorAll("li:nth-child(odd)");
 
-for (let i = 0; i < odd.length; i += 1) {
-    odd[i].style.backgroundColor = "lightgray";
-}
+// function makeItColor(li) {
+//     const myList = document.getElementsByTagName("li");
+
+//     for (let i = 0; i < myList.length; i += 1) {
+//         myList[i].style.color = "red";
+//     }
+
+//     const odd = document.querySelectorAll("li:nth-child(odd)");
+
+//     for (let i = 0; i < odd.length; i += 1) {
+//         odd[i].style.backgroundColor = "lightskyblue";
+//     }
+// }
 
 const descriptionInput = document.querySelector("input.description");
 const descriptionP = document.querySelector("p.description");
@@ -48,14 +68,42 @@ addItemButton.addEventListener("click", () => {
     let ul = document.getElementsByTagName("ul")[0];
     let li = document.createElement("li");
     li.textContent = addItemInput.value;
+    attachListItemButtons(li);
     ul.appendChild(li);
     addItemInput.value = "";
 });
 
-const removeItemButton = document.querySelector("button.removeItemButton");
+const listUl = listDiv.querySelector("ul");
 
-removeItemButton.addEventListener("click", () => {
-    let ul = document.getElementsByTagName("ul")[0];
-    let li = document.querySelector("li:last-child");
-    ul.removeChild(li);
+listUl.addEventListener("click", (event) => {
+    if (event.target.tagName == "BUTTON") {
+        if (event.target.className == "remove") {
+            let li = event.target.parentNode;
+            let ul = li.parentNode;
+            ul.removeChild(li);
+        }
+        if (event.target.className== "up") {
+            let li = event.target.parentNode;
+            let prevLi = li.previousElementSibling;
+            let ul = li.parentNode;
+            if (prevLi) {
+                ul.insertBefore(li, prevLi);
+            }
+        }
+        if (event.target.className== "down") {
+            let li = event.target.parentNode;
+            let nextLi = li.nextElementSibling;
+            let ul = li.parentNode;
+            if (nextLi) {
+                ul.insertBefore(nextLi, li);
+        }
+        }
+    }
 });
+
+
+// const firstListItem = listUl.firstElementChild;
+// const lastListItem = listUl.lastElementChild;
+
+// firstListItem.style.backgroundColor = "lightskyblue";
+// lastListItem.style.backgroundColor = "lightsteelblue";
